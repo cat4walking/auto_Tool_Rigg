234e304a18435e86da73505e3d30d13a0b323b1d
cat4walking <lethangg1998@gmail.com>
2022-10-25 15:29:35 +0700

    fixed position tacticle/ feature: bind skin fit total height spine

--- autoRigg.py
+++ autoRigg.py
@@ -1,63 +1,69 @@
 from select import select
 import maya.cmds as cmds
-editMode = True
 
 cmds.window("GearInc AutoTool")
-cmds.rowColumnLayout(numberOfColumns = 4 )
+cmds.rowColumnLayout(numberOfColumns = 1 )
 cmds.button(l = "Create Locators", w = 200, c = "createLocators()")
-cmds.button(l = "Edit Mode", w = 200, c = "lockAll(editMode)")  
-cmds.button(l = "Bind Skin", w = 200, c = "bindSkin()")
 cmds.button(l = "Create Joints", w = 200, c = "createJoints()")
+cmds.button(l = "Create Tacticile", w = 200, c = "createTacticle()")
 cmds.button(l = "Create FK", w = 200, c = "createCurvesFK()")
 cmds.button(l = "Create IK", w = 200, c = "createCurvesIK()")
-cmds.button(l = "Create Tacticile", w = 200, c = "createTacticle()")
+cmds.button(l = "Bind Skin", w = 200, c = "bindSkin()")
 cmds.button(l = "Delete", w = 200, c = "delete()")
 
 cmds.separator()
 cmds.separator()
 
 cmds.rowColumnLayout(numberOfRows  = 4, w = 200)
+
 cmds.text("Spine Count", l = "Spine Count")
 spineCount = cmds.intField(minValue = 1, maxValue = 100, value = 4)
 
-cmds.text("Height Tacticle", l = "Height Tacticle")
-heightTacticle =  cmds.intField(minValue = 1, maxValue = 10, value = 1)
+cmds.text("Height Count", l = "Height Count")
+count = cmds.intField(minValue = 1, maxValue = 100, value = 2)
+
+cmds.text("Thickness", l = "Thickness")
+thickNess = cmds.intField(minValue = 1, maxValue = 10, value = 1)
+
 
 
 cmds.showWindow()
 
-
+#function create locators
 def createLocators():
     if cmds.objExists("Gear_Master"):
         print("Gear_Master is already exists") 
     else :  cmds.group(em = True, name = "Gear_Master")
-    root = cmds.spaceLocator(n = "Gear_Root")
+    root = cmds.spaceLocator(n = "GEAR_SPINE")
     cmds.scale(0.1, 0.1, 0.1, root)
-    cmds.move(0, 1, 0, root)
+    cmds.move(0, 0, 0, root)
     cmds.parent(root, "Gear_Master")
-
 
     createSpine()
 
-
 # function create spine
 def createSpine():
-    for i in range(0, cmds.intField(spineCount, query = True, value = True)):
+    global height
+    for i in range(0, cmds.intField(count, query = True, value = True)):
         print(i)
+    height = i+1
+
+    for i in range(0, cmds.intField(spineCount, query = True, value = True)):
         spine = cmds.spaceLocator(n = "Gear_Spine" + str(i))
         cmds.scale(0.1, 0.1, 0.1, spine)
-        if i == 0 : cmds.parent(spine, "Gear_Root")
+        cmds.move(0, height + i, spine)
+        if i == 0 : cmds.parent(spine, "GEAR_SPINE")
         else : cmds.parent(spine, "Gear_Spine" + str(i - 1))
-        cmds.move(0, 1.25 + (0.25 * i), spine)
+        cmds.move(0, height + (i * height), spine)
 
-
+#function create joints
 def createJoints():
     if cmds.objExists("RIG"):
         print("Rig allready exists")
     else:
         jointGRP =  cmds.group(em = True, name = "RIG")
         cmds.move(0, 1, 0, jointGRP)
-        root = cmds.ls("Gear_Root")
+        root = cmds.ls("GEAR_SPINE")
         #eliminate shapes
         allSpinesNoShapes = cmds.listRelatives("Gear_Spine*", shapes = False, type="locator")
         
@@ -68,17 +74,17 @@ def createJoints():
         global j
         for i, s in enumerate(spine):
             pos = cmds.xform(s, q = True, t = True, ws = True)
-            j = cmds.joint(radius = 0.08, p = pos, name = "RIG_SPINE_" + str(i))
+            j = cmds.joint(radius = 0.1, p = pos, name = "RIG_SPINE_" + str(i))
                                    
 
-#function create controller IK
+#function create controller FK
 def createCurvesFK():
         sl = cmds.ls("RIG_SPINE_*")
 
-        ctrlFK  = cmds.circle( nr=(0, 1, 0), r=0.3,  name = "CURVE_SPINE_FK")   
+        ctrlFK  = cmds.circle( nr=(0, 1, 0), r=2,  name = "CURVE_SPINE_FK")   
         cmds.matchTransform("CURVE_SPINE_FK" ,"RIG_SPINE" , piv = True , pos = True)
         cmds.parentConstraint(ctrlFK, "RIG_SPINE", mo = True)
-        group = cmds.group(em = True, name = "Controller_FK")
+        cmds.group(em = True, name = "Controller_FK")
         cmds.parent(ctrlFK, "Controller_FK")
         for i, s in enumerate(sl):
             ctrlName = s.replace("_jnt", "_ctrl")
@@ -93,21 +99,28 @@ def createCurvesFK():
 def createCurvesIK():  
     cmds.ikHandle (sj="RIG_SPINE", ee= j)
 
+#function count element in list
+def countElement():
+    all = cmds.listRelatives("Gear_Spine*", shapes = False, type="locator")
+    global allCount 
+    allCount = 0
+    for element in all:
+        allCount += 1      
+    return allCount
+
 #function create mesh
-
 def createTacticle():
-    global height
-    # global tnTacticle
+    countElement()
+    totalHeight = height * allCount
 
-    for i in range(0, cmds.intField(heightTacticle, query = True, value = True)):
+    for i in range(0, cmds.intField(thickNess, query = True, value = True)):
         print(i)
-    height = i
-    tacticle = cmds.polyCylinder(h = height, n = "Tacticle", sy = 50, radius = 1)
-    test = cmds.move(0, float(tacticle), 0, "Tacticle")
-    print(test)
+    radius = i + 1
+    # global tnTacticle
+    cmds.polyCylinder(h = totalHeight, n = "Tacticle", sy = 50, radius = radius)
+    cmds.move(0, allCount, 0, "Tacticle")
 
 #fuction bind
-
 def bindSkin():
     joints = cmds.ls("RIG_SPINE*")
     print(joints)
@@ -116,26 +129,6 @@ def bindSkin():
 
     cmds.bindSkin(mesh, joints)
 
-#function lockAll
-
-def lockAll(lock):
-    axis = ["x", "y", "z"]
-    attr = ["t", "r", "s"]
-    nodes = cmds.listRelatives("Gear_*", allParents = True)
-
-    for axe in axis:
-        #print(axe)
-        for att in attr:
-            #print(att)
-            for node in nodes:
-                cmds.setAttr(node +"." +att+axe, lock = lock)
-                cmds.setAttr(node + "." +"t"+"z" , lock = lock)
-                cmds.setAttr(node + "." +"r"+"z" , lock = lock)
-                cmds.setAttr(node + "." +"s"+"z" , lock = lock)
-
-    if editMode == False:
-        editMode = True
-    else: editMode = False    
 
 def delete():
     nodes = cmds.ls("Gear_*")
